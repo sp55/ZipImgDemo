7b726409e9aded4767205d1ea0f5b6e58d518460
UILabel <1172242456@qq.com>
2016-06-05 19:13:35 +0800

    压缩图片
    压缩图片

---
+++ ZipImgDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZipImgDemo
+//
+//  Created by admin on 16/6/5.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
